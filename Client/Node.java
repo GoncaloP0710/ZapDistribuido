34f1ee491a1c6c135b3e28ab1a5c6b57066a022c
Goncalo Pinto <fc58178@alunos.fc.ul.pt>
2024-09-20 21:31:43 +0100

    Updated some stuff on the Node part

--- Client/Node.java
+++ Client/Node.java
@@ -13,11 +13,14 @@ public class Node {
     
     private String ip;
     private int port;
+
+    private int hashLength = 160; // Length of the hash in bits (SHA-1)
     private BigInteger hash; // Hash of the ip and port to identify the node order
 
-    public Node(String ip, int port) {
+    public Node(String ip, int port) throws NoSuchAlgorithmException {
         this.ip = ip;
         this.port = port;
+        setHashNumber(calculateHash());
     }
 
     public ArrayList<Node> getFingerTable() {
@@ -40,12 +43,25 @@ public class Node {
         this.fingerTable = fingerTable;
     }
 
+    private void setPort(int port) {
+        this.port = port;
+    }
+
+    private void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    private void setHashNumber(BigInteger hash) {
+        this.hash = hash;
+    }
+
     /**
-     * Calculates the hash of the node and updates its value
      * 
+     * 
+     * @return
      * @throws NoSuchAlgorithmException
      */
-    private void calculateHash() throws NoSuchAlgorithmException {
+    private BigInteger calculateHash() throws NoSuchAlgorithmException {
         String msg = ip + port;
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         byte[] hashBytes = md.digest(msg.getBytes());
@@ -54,7 +70,26 @@ public class Node {
         BigInteger hashNumber = new BigInteger(1, hashBytes);
         System.out.println("Hash as Decimal Number: " + hashNumber);
 
-        this.hash = hashNumber;
+        return hashNumber;
+    }
+
+    /**
+     * Obtains the updated finger table of the node
+     * 
+     * @param counter initializad as 0
+     * @param node
+     * @param fingerTable
+     * @return
+     */
+    private ArrayList<Node> getUpdateFingerTable(int counter, Node node, ArrayList<Node> fingerTable) {
+        if (this.equals(node) || counter == this.hashLength - 1)
+            return fingerTable;
+        // TODO: Change from the hash number value to the interval of the hashs between these and the last node
+        if (node.hash.intValue() >= Math.pow(2, counter)) {
+            fingerTable.add(node);
+            return getUpdateFingerTable(counter + 1, node.getFingerTable().get(0), fingerTable);
+        }
+        return getUpdateFingerTable(counter, node.getFingerTable().get(0), fingerTable);
     }
     
     @Override
@@ -76,7 +111,10 @@ public class Node {
         StringBuilder sb = new StringBuilder();
         sb.append("FingerTable: [");
         for (Node node : fingerTable) {
-            sb.append(node.toString()).append(", ");
+            sb.append("IP: ").append(node.getIp())
+                .append(", Port: ").append(node.getPort())
+                .append(", Hash: ").append(node.getHashNumber())
+                .append(", ");
         }
         if (!fingerTable.isEmpty())
             sb.setLength(sb.length() - 2); // Remove the last comma and space
@@ -117,12 +155,45 @@ public class Node {
         Node node3 = new Node(ipAddress, 8083);
         Node node4 = new Node(ipAddress, 8084);
         Node node5 = new Node(ipAddress, 8085);
+        Node node6 = new Node(ipAddress, 8086);
+        Node node7 = new Node(ipAddress, 8087);
 
-        // create the hashes of the nodes
-        node1.calculateHash();
-        node2.calculateHash();
-        node3.calculateHash();
-        node4.calculateHash();
-        node5.calculateHash();
+        // Set Custom hash values for testing purposes
+        node1.setHashNumber(new BigInteger("0"));
+        node2.setHashNumber(new BigInteger("1"));
+        node3.setHashNumber(new BigInteger("3"));
+        node4.setHashNumber(new BigInteger("7"));
+        node5.setHashNumber(new BigInteger("9"));
+        node6.setHashNumber(new BigInteger("11"));
+        node7.setHashNumber(new BigInteger("14"));
+
+        // Create the finger tables first position (successor)
+        // node1 -> node2 -> node3 -> node4 -> node5 -> node6 -> node7 -> node1
+        // A ser implementado! 
+        node1.setFingerTable(new ArrayList<>() {{add(node2);}});
+        node2.setFingerTable(new ArrayList<>() {{add(node3);}});
+        node3.setFingerTable(new ArrayList<>() {{add(node4);}});
+        node4.setFingerTable(new ArrayList<>() {{add(node5);}});
+        node5.setFingerTable(new ArrayList<>() {{add(node6);}});
+        node6.setFingerTable(new ArrayList<>() {{add(node7);}});
+        node7.setFingerTable(new ArrayList<>() {{add(node1);}});
+
+        // Update the finger tables
+        node1.setFingerTable( node1.getUpdateFingerTable(0, node1.fingerTable.get(0), new ArrayList<>()) );
+        node2.setFingerTable( node2.getUpdateFingerTable(0, node2.fingerTable.get(0), new ArrayList<>()) );
+        node3.setFingerTable( node3.getUpdateFingerTable(0, node3.fingerTable.get(0), new ArrayList<>()) );
+        node4.setFingerTable( node4.getUpdateFingerTable(0, node4.fingerTable.get(0), new ArrayList<>()) );
+        node5.setFingerTable( node5.getUpdateFingerTable(0, node5.fingerTable.get(0), new ArrayList<>()) );
+        node6.setFingerTable( node6.getUpdateFingerTable(0, node6.fingerTable.get(0), new ArrayList<>()) );
+        node7.setFingerTable( node7.getUpdateFingerTable(0, node7.fingerTable.get(0), new ArrayList<>()) );
+
+        // Print the finger tables
+        System.out.println("Node 1: " + node1.toString());
+        System.out.println("Node 2: " + node2.toString());
+        System.out.println("Node 3: " + node3.toString());
+        System.out.println("Node 4: " + node4.toString());
+        System.out.println("Node 5: " + node5.toString());
+        System.out.println("Node 6: " + node6.toString());
+        System.out.println("Node 7: " + node7.toString());
     }
 }
--- Handlers/NodeService.java
+++ Handlers/NodeService.java
@@ -1,54 +1,14 @@
 package Handlers;
 
-import java.util.ArrayList;
-
 import Client.Node;
 
 /**
- * This class is responsible for handling the node operations on a network base
- * Singleton pattern is used to ensure that only one instance of the class is created
+ * This class is responsible for routing the node operations
  */
 public class NodeService {
 
-    // Private static instance of the class
-    private static NodeService instance;
-
-    // Private constructor to prevent instantiation
-    private NodeService() {
+    public NodeService() {
     }
 
-    // Public method to provide access to the instance
-    public static synchronized NodeService getInstance() {
-        if (instance == null) {
-            instance = new NodeService();
-        }
-        return instance;
-    }
-
-    /**
-     * Enters a node into the network
-     * 
-     * @param node
-     */
-    private void enterNode(Node node, Node prevNode) {
-        if (prevNode == null)
-            return;
-
-        // Update the finger table of the previous node
-
-    }
-
-    /**
-     * Obtains the updated finger table of the node
-     * 
-     * @param counter 
-     * @param node
-     * @param fingerTable
-     * @return
-     */
-    private ArrayList<Node> getUpdateFingerTable(int counter, Node currentNode, Node node, ArrayList<Node> fingerTable) {
-        if (currentNode.equals(node))
-            return fingerTable;
-        // TODO: Implement the logic to update the finger table
-    }
+    // TODO: Implement a router/Driver class to handle the node operations (from the interface to the methods)
 }
