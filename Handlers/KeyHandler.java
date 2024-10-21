382ffa3cf4349a9cabd1109d4bd8408940d147c1
André Reis <bakugan.erdna@gmail.com>
2024-10-21 20:08:51 +0100

    cocó

--- Handlers/KeyHandler.java
+++ Handlers/KeyHandler.java
@@ -28,16 +28,25 @@ public class KeyHandler {
         System.out.println("KeyHandler constructor");
         this.keyStorePassword = keyStorePassword;
         this.keyStoreString = keystoreString;
-        this.keystoreFile = new File("files/"+keystoreString+".jks"); 
-        this.certificateFile = new File("files/"+keystoreString +".cer");
-        this.trustStoreFile = new File("files/"+keystoreString+"_TrustStore.jks");
+
+        String keystoreFilePath = "files/" + keystoreString + ".jks";
+        String certificateFilePath = "files/" + keystoreString + ".cer";
+        String trustStoreFilePath = "files/" + keystoreString + "_TrustStore.jks";
+
+        // Initialize File objects using the paths
+        this.keystoreFile = new File(keystoreFilePath);
+        this.certificateFile = new File(certificateFilePath);
+        this.trustStoreFile = new File(trustStoreFilePath);
+
         initialize(); 
+
         System.out.println("certificate trustsore: "+ trustStore.getCertificate(keystoreString));
         System.out.println("certificate keystore: "+ keyStore.getCertificate(keystoreString));
         System.out.println("keystore aliases: "+ keyStore.aliases());
         System.out.println("keyStore: "+ keyStore);
         System.out.println("keyStore key: "+ keyStore.getKey(keystoreString, keyStorePassword.toCharArray()));
         System.out.println("keystoreFile: "+ keystoreFile);
+
         this.certificate = getCertificate(keystoreString);
 
     }
@@ -112,12 +121,10 @@ public class KeyHandler {
         this.trustStore = ts;
     }
 
-    public void firstTimeUser() throws Exception{
-
-
+    public void firstTimeUser() throws Exception {
         keyStore = KeyStore.getInstance("JKS");
 
-        //load keystore
+        // Load keystore
         keyStore.load(null, keyStorePassword.toCharArray());
 
         File filesDir = new File("files");
@@ -125,108 +132,116 @@ public class KeyHandler {
             filesDir.mkdirs();
         }
 
-         // Create keystore file path
-         String keystoreFilePath = "files/" + keyStoreString + ".jks";
+        // Create keystore file path
+        String keystoreFilePath = "files/" + keyStoreString + ".jks";
+        this.keystoreFile = new File(keystoreFilePath);
 
-         try (FileOutputStream fos = new FileOutputStream(keystoreFile)) {
-            keyStore.store(fos, keyStorePassword.toCharArray());
-            fos.close();
-        }
-
-        keyStore.load(null);
-
-        //create keystore
-        String[] args = new String[]{//"/bin/bash", "-c",
+        // Create keystore
+        String[] args = new String[]{
             "keytool", "-genkeypair", "-alias", keyStoreString, "-keyalg", "RSA", "-keysize", "2048",
             "-validity", "365", "-keystore", keystoreFilePath, "-storepass", keyStorePassword,
-            "-dname", "CN=a OU=a O=a L=a ST=a C=a", "-storetype", "JKS" //ainda sussy
+            "-dname", "CN=a OU=a O=a L=a ST=a C=a", "-storetype", "JKS"
         };
-        Process proc = new ProcessBuilder(args).start(); 
-        proc.waitFor(1, TimeUnit.SECONDS); //precisamos?
-          
+
+        Process proc = new ProcessBuilder(args).start();
+        int exitCode = proc.waitFor(); // Wait for the process to complete
+
+        if (exitCode != 0) {
+            throw new RuntimeException("Keystore creation process failed with exit code: " + exitCode);
+        }
+
         System.out.println("Keystore created");
         try (FileInputStream fis = new FileInputStream(keystoreFile)) {
             System.out.println("Loading keystore");
             keyStore.load(fis, keyStorePassword.toCharArray());
             System.out.println("Keystore loaded");
-            fis.close();
         }
 
-        this.keystoreFile = new File(keystoreFilePath); 
-
-
-
-        //create certificate File
+        // Create certificate file
         createCertificate();
         String certificateFilePath = "files/" + keyStoreString + ".cer";
 
-
-        //load trustStore
+        // Load trustStore
         trustStore = KeyStore.getInstance("JKS");
         trustStore.load(null, keyStorePassword.toCharArray());
 
-        String trustStoreFilePath = "files/" + keyStoreString + "_TrustStore" + ".jks";
-
-        try (FileOutputStream fos2 = new FileOutputStream(trustStoreFile)) {
-            trustStore.store(fos2, keyStorePassword.toCharArray());
-            fos2.close();
-        }
-        
-        System.out.println("Truststore created");
-        System.out.println("Truststore loading");
-        trustStore.load(null);
-
-        //create truststore File
-        String[] argsTrust = new String[]{
-            "keytool", "-import", "-alias", keyStoreString, "-file", certificateFilePath, 
-            "-storetype", "JKS","-keystore", trustStoreFilePath //ainda sussy
-        };
-        Process procTrust = new ProcessBuilder(argsTrust).start(); 
-        procTrust.waitFor(1, TimeUnit.SECONDS); //precisamos?
+        String trustStoreFilePath = "files/" + keyStoreString + "_TrustStore.jks";
         this.trustStoreFile = new File(trustStoreFilePath);
 
+        // Save the initial truststore
+        try (FileOutputStream fos2 = new FileOutputStream(trustStoreFile)) {
+            trustStore.store(fos2, keyStorePassword.toCharArray());
+        }
+
+        System.out.println("Truststore created");
+
+        // Import certificate into truststore
+        String[] argsTrust = new String[]{
+            "keytool", "-import", "-alias", keyStoreString, "-file", certificateFilePath,
+            "-storetype", "JKS", "-keystore", trustStoreFilePath, "-noprompt", "-storepass", keyStorePassword
+        };
+
+        Process procTrust = new ProcessBuilder(argsTrust).start();
+        int exitCodeTrust = procTrust.waitFor(); // Wait for the process to complete
+
+        if (exitCodeTrust != 0) {
+            throw new RuntimeException("Truststore import process failed with exit code: " + exitCodeTrust);
+        }
 
         System.out.println("Truststore loaded");
 
+        // Load the updated truststore
         try (FileInputStream fis2 = new FileInputStream(trustStoreFile)) {
             System.out.println("Loading truststore");
             trustStore.load(fis2, keyStorePassword.toCharArray());
             System.out.println("Truststore loaded 2");
-            fis2.close();
         }
 
-
-        addCertificateToTrustStore(keyStoreString, keyStore.getCertificate(keyStoreString));
-
-        
+        addCertificateToTrustStore(keyStoreString, keyStore.getCertificate(keyStoreString), this.trustStoreFile);
     }
 
-    public void createCertificate() throws Exception{
+    public void createCertificate() throws Exception {
         String certificateFilePath = "files/" + keyStoreString + ".cer";
+
         String[] argsCert = new String[]{
-            "keytool", "-exportcert", "-alias", keyStoreString, "-storetype", "JKS", "-keystore", 
-            "files/"+keyStoreString + ".jks", "-file", certificateFilePath // caminho completo
+            "keytool", "-exportcert", "-alias", keyStoreString, "-storetype", "JKS", "-keystore",
+            "files/" + keyStoreString + ".jks", "-file", certificateFilePath
         };
 
+        Process procCert = new ProcessBuilder(argsCert).start();
+        int exitCodeCert = procCert.waitFor(); // Wait for the process to complete
 
-        Process procCert = new ProcessBuilder(argsCert).start(); 
-        procCert.waitFor(1, TimeUnit.SECONDS); //precisamos?
+        if (exitCodeCert != 0) {
+            throw new RuntimeException("Certificate export process failed with exit code: " + exitCodeCert);
+        }
+
         this.certificateFile = new File(certificateFilePath);
 
+        if (!this.certificateFile.exists()) {
+            throw new RuntimeException("Certificate file was not created: " + certificateFilePath);
+        }
+
+        System.out.println("Certificate created at: " + certificateFilePath);
     }
 
-
-    public void addCertificateToTrustStore(String username, Certificate cer) throws Exception{
+    public void addCertificateToTrustStore(String username, Certificate cer, File trustStoreFile) throws Exception {
         System.out.println("Adding certificate to truststore");
+
+        // Load the truststore
+        try (FileInputStream fis2 = new FileInputStream(trustStoreFile)) {
+            trustStore.load(fis2, keyStorePassword.toCharArray());
+            System.out.println("Truststore loaded");
+        }
+
+        // Add the certificate to the truststore
         trustStore.setCertificateEntry(username, cer);
         System.out.println("Certificate added to truststore");
 
-        try (FileInputStream fis2 = new FileInputStream("files/" + keyStoreString + "_TrustStore" + ".jks")) {
-            trustStore.load(fis2, keyStorePassword.toCharArray());
-            fis2.close();
+        // Save the updated truststore
+        try (FileOutputStream fos = new FileOutputStream(trustStoreFile)) {
+            trustStore.store(fos, keyStorePassword.toCharArray());
+            System.out.println("Truststore saved");
         }
-        System.out.println("Truststore loaded 3");
-        
     }
 
     public PublicKey getPublicKey(String alias) throws Exception {
